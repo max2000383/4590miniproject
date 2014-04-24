99552f8c9ca98af01b549ceed520dc9dd4ddfd55
emorphis <emorphis3@gatech.edu>
2014-04-23 21:50:09 -0400

    added cave

---
+++ Assets/Scripts/Cave.js
@@ -0,0 +1,22 @@
+﻿var player : GameObject;
+private var freshApproach : boolean = true;
+var sound : AudioClip;
+private var audioSource;
+
+function Start () {
+	audioSource = player.AddComponent(AudioSource);
+}
+
+function Update () {
+	var distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+		if (!audio.isPlaying) {
+			// only have the sound play again if the player has left the cave area and returned
+			if (distance < 15.0 && freshApproach == true) {
+				freshApproach = false;
+				audioSource.PlayOneShot(sound);
+			}
+			else if (distance > 15.0 && freshApproach == false) {
+				freshApproach = true;
+			}
+		}
+}
--- Assets/Scripts/Identification.js
+++ Assets/Scripts/Identification.js
@@ -1,15 +1,16 @@
 ﻿var sound : AudioClip;
 var player : GameObject;
+private var audioSource;
 
 function Start () {
-
+	audioSource = player.AddComponent(AudioSource);
 }
 
 function Update () {
 	if (Input.GetKeyDown(KeyCode.I)) {
 		var distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
 		if (distance < 5.0) {
-			audio.PlayOneShot(sound);
+			audioSource.PlayOneShot(sound, .5);
 		}
 	}
 }
